cf61a8c946c1e8555f971f8cb2987e4653aa8811
Phatkanin <40383538+Phatkanin@users.noreply.github.com>
2018-06-19 17:06:40 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -9,7 +9,7 @@ $access_token = 'LsaJUEDZUoXs2tUNWQtnP+N1Tqva/lc0iqNtld1Boc7ByXl50abFcKJtxv5a23S
 $channelSecret = '48003d336a991184dcc5c8ba84e843cf';
 
 $pushID = 'U08c18e040bf6df694d6e3cdd07cf6431'; //เครื่องadmin
-$pushID = 'U29dc4c45a04b287d60bde5734fc6e1ba'; //เครื่องพี่ยศ
+//$pushID = 'U29dc4c45a04b287d60bde5734fc6e1ba'; //เครื่องพี่ยศ
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
