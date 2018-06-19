27508da57ad3cc639eb7891e1c0a8ba83b867c7c
Phatkanin <40383538+Phatkanin@users.noreply.github.com>
2018-06-19 17:16:22 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,6 +18,14 @@
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
         replyMsg($arrayHeader,$arrayPostData);
     }
+
+    #ตัวอย่าง Message Type "Hi"
+    else if($message == "hi"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "Hello world";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     #ตัวอย่าง Message Type "Sticker"
     else if($message == "ฝันดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
