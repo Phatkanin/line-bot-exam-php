97d48a40ab160adbeac764efec04e1b748ce66a1
Phatkanin <40383538+Phatkanin@users.noreply.github.com>
2018-06-19 15:46:57 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,11 +4,12 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'J6bNuT1wEIjZp8uwi47Z91mcpgtOQE6htEO8T2qWEj0+hLNPXKJbfRiNeBKIGFSswvKvo2Zxyn0FIuOOmUoDdRLu1zHdZjG1+gNU4rhTKBwoyjNiL2vt/cgSrzeq+SNvG1FuW2r2HaN+Gr+/NA8tXAdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '48003d336a991184dcc5c8ba84e843cf';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+//$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U08c18e040bf6df694d6e3cdd07cf6431';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
